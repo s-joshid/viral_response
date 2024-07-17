1a41092b78bfcce3b1e7655fa90611fd41c67dc8
s-joshid <j.dhruvi89@gmail.com>
2024-07-16 17:49:29 -0700

    updating blast.sh script

--- scripts/blastx.sh
+++ scripts/blastx.sh
@@ -5,27 +5,30 @@
 
 
 #working with only WH8109
-WORKDIR=$(echo '../data') #will be scripts dir
-CYCOGBLASTDIR=$(echo '../data/cycog6')
-GENOMENAME=$(echo 'WH8109')
+WORKDIR=$(realpath '../data') #will be in at first scripts dir
+CYCOGBLASTDIR="${WORKDIR}/img_data_cycog6"
+GENOMENAME="WH8109"
 
-cd ${WORKDIR}/blastoutput
 
 #making FASTA file of WH1809 coding sequece
-
-cat ${WORKDIR}/WH8109sequence.txt | sed -r 's/^>.+locus_tag=/>/' | sed 's/[]] .+$//' > ${WORKDIR}/${GENOMENAME}.cds.fna  
+echo "WORKDIR: ${WORKDIR}"
+echo "GENOMENAME: ${GENOMENAME}"
+cat ${WORKDIR}/WH8109sequence.txt | sed -r 's/^>.+locus_tag=/>/' | sed -r 's/[]] .+$//' > ${WORKDIR}/${GENOMENAME}.cds.fna
 
 #making sure blastx biocontainer is installed
-if [! '../containers/blastx.sif'];
-    then
+if [ ! -e '../containers/blastx.sif' ];
+then
     cd ../containers
     singularity build blastx.sif docker://quay.io/biocontainers/blast:2.14.1--pl5321h6f7f691_0
-    cd ../scripts
+fi
+
 
 #Find cycog6 matches for each WH8109
-singularity exec --no-home --bind ../:Viral_response ../containers/blastx.sif \
-    blastx -db ${CYCOGBLASTDIR} -query ${WORKDIR}/${GENOMENAME}.cds.fna  \
-    -out ${WORKDIR}/${GENOMENAME}_blastx_cycog6.out
+
+singularity exec --no-home --bind "${WORKDIR}":"${WORKDIR}" ../containers/blastx.sif \
+    blastx -db "${CYCOGBLASTDIR}" \
+    -query "${WORKDIR}/${GENOMENAME}.cds.fna"  \
+    -out "${WORKDIR}/${GENOMENAME}_blastx_cycog6.out" \
     -evalue 0.001 \
     -num_threads 20 \
     -outfmt "6 qseqid sseqid evalue bitscore pident qcovs qcovhsp"
@@ -35,11 +38,3 @@ cat ${WORKDIR}/${GENOMENAME}_blastx_cycog6.out | awk - F "\t" '!seen[$1]++' > ${
 #table of legacy genome tags and best Cycog ID match
 cat ${WORKDIR}/${GENOMENAME}_blastx_cycog6_tophit.out | awk 'BEGIN {FS="\t"} ; {OFS="\t"} ; {print $1,$2}' > ${WORKDIR}/${GENOMENAME}_cycog6_table.tsv 
 sed -i -r 's/\|/\t/' ${WORKDIR}/${GENOMENAME}_cycog6_table.tsv
-
-
-
-
-
-
-
-
